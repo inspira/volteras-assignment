8bab5636c7678c2a196f58642fdc63d03f65e52a
Thiago Alves Silva <thiago.alves@inspira.com.br>
2022-09-20 13:50:45 -0300

    [main] Git commit hooks for static code analysis + Added a Model to the API (still with dummy data) + documentation stub

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,46 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Table from './Table';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
 
 const columns = [
-  { accessor: 'timestamp', label: 'Timestamp' },
-  { accessor: 'speed', label: 'Speed (km/h)' },
-  { accessor: 'odometer', label: 'Odometer (km)' },
-  { accessor: 'soc', label: 'State of Charge', format: (value) => `${value}%` },
-  { accessor: 'elevation', label: 'Elevation (m)' },
-  { accessor: 'shift_state', label: 'Shift State' },
+  { accessor: "timestamp", label: "Timestamp" },
+  { accessor: "speed", label: "Speed (km/h)" },
+  { accessor: "odometer", label: "Odometer (km)" },
+  { accessor: "soc", label: "State of Charge", format: (value) => `${value}%` },
+  { accessor: "elevation", label: "Elevation (m)" },
+  { accessor: "shift_state", label: "Shift State" },
 ];
 
 // TODO: move to a config file
-const API_PATH = '//localhost:8000/api/v1';
+const API_PATH = "//localhost:8000/api/v1";
 
 function App() {
   const [data, setData] = useState();
+  const [error, setError] = useState();
 
   function loadData() {
     fetch(`${API_PATH}/vehicle_data`)
       .then((response) => response.json())
-      .then((jsonData) => setData(jsonData));
+      .then((jsonData) => setData(jsonData))
+      .catch((err) => {
+        setError(`Failed to load data: ${err}`);
+      });
   }
 
   // This is calling the API twice because of a new behavior of
   // React 18 when React.StrictMode is enabled in development mode
-  // A fix is provided at https://beta.reactjs.org/learn/synchronizing-with-effects
+  // See also: https://beta.reactjs.org/learn/synchronizing-with-effects
   useEffect(() => {
     loadData();
   }, []);
 
-  return data && (
+  return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Volteras Vehicle Data
-        </h1>
+        <h1>Volteras Vehicle Data</h1>
       </header>
-      <Table rows={data} columns={columns} />
+      <Table rows={data ?? []} columns={columns} />
+      <p>{error}</p>
     </div>
   );
 }
